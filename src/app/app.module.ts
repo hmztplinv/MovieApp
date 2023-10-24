98174a722e5a4edc33c9dec2749a9f0f1eb8bc67
hmztplinv <hmz.tpal@gmail.com>
2023-10-24 15:17:27 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoryComponent } from './components/category/category.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieComponent } from './components/movies/movie/movie.component';
+import { MovieDetailsComponent } from './components/movies/movie-details/movie-details.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
